81692beed825b114e39f8f65cd4236a83092b406
Hossein Tavakolian <tavakolianh@gmail.com>
2015-07-28 18:58:57 +0430

    corrects mean calculation

--- manTotal.m
+++ manTotal.m
@@ -1,19 +1,25 @@
 clear all
 load 'Output\Total.mat';
 A.x24=cellfun(@(x) x(6:end),A.x5,'uniformoutput', false);
-A.x25=cellfun(@(x) x(6:7),A.x5,'uniformoutput', false);
-C=grpstats(A,{'x24','x2'},'mean','DataVars','x8');
+C=grpstats(A,{'x24','x2'},{'sum','mean'},'DataVars',{'x7', 'x8'});
 % C(:,'GroupCount') = [];
 C.Properties.ObsNames=[];
 C.Properties.VarNames{1} = 'Date';
 C.Properties.VarNames{2} = 'District';
 C.Properties.VarNames{3} = 'NumTrans';
-C.Properties.VarNames{4} = 'Mean';
-D=grpstats(C,'Date','mean','DataVars','Mean');
+C.Properties.VarNames{4} = 'TotalValue';
+C.Properties.VarNames{5} = 'MeanValue';
+C.Properties.VarNames{6} = 'SumPrice';
+C.Properties.VarNames{7} = 'Mean';
+D=grpstats(A,{'x24'},{'sum','mean'},'DataVars',{'x7', 'x8'});
 % D(:,'GroupCount') = [];
 D.Properties.ObsNames=[];
+D.Properties.VarNames{1} = 'Date';
 D.Properties.VarNames{2} = 'NumTrans';
-D.Properties.VarNames{3} = 'Mean';
+D.Properties.VarNames{3} = 'TotalValue';
+D.Properties.VarNames{4} = 'MeanValue';
+D.Properties.VarNames{5} = 'SumPrice';
+D.Properties.VarNames{6} = 'Mean';
 D.District=zeros(size(D,1),1);
 C=[C;D];
 clear A B D
@@ -36,4 +42,5 @@ for i=0:22
     HousePrice.(['District' num2str(i)]).Pgrowth=Pgrowth;
     HousePrice.(['District' num2str(i)]).Stat=Stat;
 end
+save('Output\HousePriceTotal.mat', 'HousePrice');
 clc
--- manTotalAge.m
+++ manTotalAge.m
@@ -2,26 +2,33 @@
 clear all
 load 'Output\Total.mat';
 A.x25=cellfun(@(x) x(6:7),A.x5,'uniformoutput', false);
-C=grpstats(A,{'x25','x2', 'x16'},'mean','DataVars','x8');
+C=grpstats(A,{'x25','x2', 'x16'},{'sum','mean'},'DataVars',{'x7', 'x8'});
 % C(:,'GroupCount') = [];
 C.Properties.ObsNames=[];
 C.Properties.VarNames{1} = 'Date';
 C.Properties.VarNames{2} = 'District';
 C.Properties.VarNames{3} = 'Age';
 C.Properties.VarNames{4} = 'NumTrans';
-C.Properties.VarNames{5} = 'Mean';
-D=grpstats(C,{'Date', 'Age'},{'mean','sum'},'DataVars',{'Mean','NumTrans'});
-D(:,{'GroupCount','mean_NumTrans','sum_Mean'}) = [];
+C.Properties.VarNames{5} = 'TotalValue';
+C.Properties.VarNames{6} = 'MeanValue';
+C.Properties.VarNames{7} = 'SumPrice';
+C.Properties.VarNames{8} = 'Mean';
+D=grpstats(A,{'x25', 'x16'},{'sum','mean'},'DataVars',{'x7', 'x8'});
+% D(:,{'mean_NumTrans','sum_Mean', 'mean_TotalValue'}) = [];
 % D(:,'GroupCount') = [];
 D.Properties.ObsNames=[];
+D.Properties.VarNames{1} = 'Date';
 D.Properties.VarNames{2} = 'Age';
-D.Properties.VarNames{3} = 'Mean';
-D.Properties.VarNames{4} = 'NumTrans';
+D.Properties.VarNames{3} = 'NumTrans';
+D.Properties.VarNames{4} = 'TotalValue';
+D.Properties.VarNames{5} = 'MeanValue';
+D.Properties.VarNames{6} = 'SumPrice';
+D.Properties.VarNames{7} = 'Mean';
 D.District=zeros(size(D,1),1);
 D = sortrows(D,'Date','ascend');
 C=[C;D];
 C = sortrows(C,'Date','ascend');
 % clear A B D
 C = sortrows(C,'Date','ascend');
-export(C,'xlsfile','ManTotalAge1');
+export(C,'xlsfile','Output\ManTotalAge1');
 clc
